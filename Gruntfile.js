ebc73d7a47f96ad0ff3717a2d46dae886b9db0fb
Osoian Marcel <osoian.marcel.d@gmail.com>
2015-07-05 00:03:39 +0300

    Change production directory name

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
 			prod: {
 				options: {
 					sassDir: 'dev/scss',
-					cssDir: 'widget',
+					cssDir: 'prod',
 					imagesDir: 'dev/img',
 					httpPath: '/',
 					environment: 'production'
